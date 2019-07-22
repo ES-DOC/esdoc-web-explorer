8a7c8b39e4201639a05c23ee39efbb4ad488deee
momipsl <momipsl@ipsl.fr>
2019-07-22 17:43:28 +0200

    1. Version bump.  2. Ensured links are to new browser tab.

--- src/components/cim2/shared/CitationDetail.vue
+++ src/components/cim2/shared/CitationDetail.vue
@@ -11,7 +11,7 @@
             <tr v-if="citation.citationDetail">
                 <td colspan="2" class="cim2-citation-detail">
                     <div v-if="citation.doi && citation.doi !== 'n/a'">
-                        <a :href=citation.doi class="cim2-citation-doi">{{ citation.citationDetail }} target="_blank"</a>
+                        <a :href=citation.doi class="cim2-citation-doi" target="_blank">{{ citation.citationDetail }}</a>
                     </div>
                     <div v-else>
                         {{ citation.citationDetail }}
